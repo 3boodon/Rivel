98c5cf485881451b59496cc3c0bb51bde3d72b3d
Abdulrhman Obaid <3bood.on@gmail.com>
2023-10-13 03:49:48 +0300

    Modified user_reservations view to make it change depending on user id

--- Database/views/user_reservations.sql
+++ Database/views/user_reservations.sql
@@ -19,6 +19,7 @@ FROM available_cars
     JOIN reservations on available_cars.car_id = reservations.car_id
     JOIN payments on payments.payment_id = reservations.payment_id
     JOIN payment_methods on payment_methods.payment_method_id = payments.payment_method_id
+    JOIN users on reservations.user_id = 9 -- this Id should be dynamically changed depending on the logged in user
 GROUP BY reservation_id
 ORDER BY `reserved_on` DESC;
 
